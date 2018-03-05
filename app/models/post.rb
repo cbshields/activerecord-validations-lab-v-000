91b4f3ce650185e1a76b5efc7a0fa2bdf9d89932
Caroline Shields <cbshields@outlook.com>
2018-03-05 22:29:16 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+  title_array = ["Won't Believe", "Secret", "Top [0-9]*", "Guess"]
+  
 end
