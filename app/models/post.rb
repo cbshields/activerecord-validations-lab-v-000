f838cc64870bea2f8a3fa135941c6a9ff7614d8c
Caroline Shields <cbshields@outlook.com>
2018-03-05 22:03:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 10}
+  validates :long_summary, length {maximum: 9}
   validates :category, inclusion: { in: %w(fiction non-fiction romance)}
 end
